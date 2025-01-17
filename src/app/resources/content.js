5450c17feceb8f4a2197b99b70dee677ff7c2663
jackmonaco20 <jackmonaco20@yahoo.com>
2025-01-17 01:28:39 -0500

    fix avatar img

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Software Engineer",
-  avatar: "/images/profile-pictures/ProfilePicWhite.png",
+  avatar: "/images/profile-pictures/avatar.jpg",
   location: "America/New_York", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: [], // optional: Leave the array empty if you don't want to display languages
 };
