fb8f9a7e31940ceb0fe409b7d61d6e725785ed93
jackmonaco20 <jackmonaco20@yahoo.com>
2025-01-24 14:53:32 -0500

    PATCH: gridiron copy + ts-ignore for gallery page

--- src/app/resources/index.ts
+++ src/app/resources/index.ts
@@ -1,3 +1,22 @@
 // import a pre-defined template for config and content options
-export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL } from '@/app/resources/config'
-export { person, social, newsletter, home, about, blog, work, gallery } from '@/app/resources/content'
+export {
+  routes,
+  protectedRoutes,
+  effects,
+  style,
+  display,
+  mailchimp,
+  baseURL,
+} from "@/app/resources/config";
+
+export {
+  person,
+  social,
+  newsletter,
+  home,
+  about,
+  blog,
+  work,
+  // @ts-ignore
+  gallery,
+} from "@/app/resources/content";
